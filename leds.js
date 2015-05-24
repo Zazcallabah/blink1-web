c12f4a08c10683246fbb8941bf742dded50a89cc
Peter Hamberg <zazcallabah@gmail.com>
2015-05-24 17:20:50 +0200

    restructure server api, add support for color patterns extract led and pattern parts into separate modules

---
+++ leds.js
@@ -0,0 +1,65 @@
+/*
+
+GET /api/leds
+Get the current led colors
+returns {ledA:"#hexcolor",ledB:"#hexcolor"}
+
+POST /api/leds
+Set the led colors
+param {ledn:0, time:0, color:"#hexcolor"}
+where ledn 0 means both leds, ledn 1 is led A, ledn 2 is led B
+time is in milliseconds
+returns ack
+
+*/
+
+var Leds = function(blink)
+{
+	this.blink = blink;
+	this.href = "/api/leds";
+};
+
+Leds.prototype.hexconvert = function(num){
+	var res = num.toString(16);
+	if( num <= 0xf )
+		return "0"+res;
+	return res;
+};
+
+Leds.prototype.get = function(response){
+	this.blink.readCurrentColor( 1, function(c1){
+		this.blink.readCurrentColor( 2, function(c2){
+			var l1 = "#" + 
+				hexconvert(c1.r) + 
+				hexconvert(c1.g) + 
+				hexconvert(c1.b);
+			var l2 = "#" + 
+				hexconvert(c2.r) + 
+				hexconvert(c2.g) + 
+				hexconvert(c2.b);
+
+			response.writeHead(200);
+			response.write( JSON.stringify( {ledA:l1,ledB:l2} ) );
+			response.end();
+		});
+	});
+};
+
+Leds.prototype.post = function(instruction, response){
+	var ledn = instruction.ledn || 0;
+	var time = instruction.time || 0;
+	var hexcolor = instruction.color || "#000000";
+	//check hexcolor valid data
+	var r = parseInt(hexcolor.substr(1,2),16);
+	var g = parseInt(hexcolor.substr(3,2),16);
+	var b = parseInt(hexcolor.substr(5,2),16);
+	
+	blink.fadeToRGB( time, r, g, b, ledn );
+
+	response.writeHead(200);
+	response.write( "set led "+ledn+" to "+hexcolor+ " over "+time+"ms" );
+	response.end();
+};
+
+
+module.exports = Leds;
---
+++ patterns.js
@@ -0,0 +1,85 @@
+/*
+
+GET /api/patterns
+Get the led color patterns
+returns [{ledn:0,color:"#hexcolor",time:0,index:0},...]
+
+POST /api/patterns
+Write color pattern sequences
+param [{ledn:0, time:0, color:"#hexcolor", index:0},...]
+where ledn 0 means both leds, ledn 1 is led A, ledn 2 is led B
+time is in milliseconds
+index is pattern line [0-31]
+returns ack
+
+*/
+
+var Patterns = function(blink)
+{
+	this.blink = blink;
+	this.href = "/api/patterns";
+};
+
+Patterns.prototype.hexconvert = function(num){
+	var res = num.toString(16);
+	if( num <= 0xf )
+		return "0"+res;
+	return res;
+};
+
+Patterns.prototype.get = function(response){
+	var count = 32;
+	var patterns = [];
+	var callback = function(data,i){
+		var pattern = {
+			ledn: data.ledn,
+			color:  "#" + 
+				hexconvert(data.r) + 
+				hexconvert(data.g) + 
+				hexconvert(data.b),
+			time: data.fadeMillis,
+			index: i
+		};
+		patterns.push(pattern);
+		count--;
+		if( count === 0 )
+		{
+			response.writeHead(200);
+			response.write( JSON.stringify( patterns ) );
+			response.end();
+			return;
+		}
+	};
+	for( var i = 0; i<=31; i++ )
+		this.blink.readPatternLine( i, function(c){callback(c,i);} );
+};
+
+Patterns.prototype.post = function( patterns, response ){
+	
+	if( ! patterns.length )
+		return;
+
+	// first make sure led isnt playing
+	this.blink.pause();
+	
+	for( var i=0; i<patterns.length; i++ )
+	{
+		var index = patterns[i].index || 0;
+		var ledn = patterns[i].ledn || 0;
+		var time = patterns[i].time || 0;
+		var hexcolor = patterns[i].color || "#000000";
+		//check hexcolor valid data
+		var r = parseInt(hexcolor.substr(1,2),16);
+		var g = parseInt(hexcolor.substr(3,2),16);
+		var b = parseInt(hexcolor.substr(5,2),16);
+
+		this.blink.setLed( ledn );
+		this.blink.writePatternLine(time,r,g,b,index);
+	}
+	response.writeHead(200);
+	response.write( "" + patterns.length + "patterns written");
+	response.end();
+};
+
+
+module.exports = Leds;
