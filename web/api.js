6f4bbdfb16e8e93aacc71d689bd7a407e0ab2102
Peter Hamberg <zazcallabah@gmail.com>
2015-06-05 08:42:14 +0200

    add gui button for slow fade

--- web/api.js
+++ web/api.js
@@ -71,7 +71,14 @@
 			{speed:parseInt(ms,10)}
 		);
 	};
-	
+	Api.prototype.fade = function(ms) {
+		webreq(
+			"POST",
+			"/api/control/slowTogl",
+			undefined,
+			{speed:parseInt(ms,10)}
+		);
+	};
 	function Pick( id, onchange )
 	{
 		var that = this;
