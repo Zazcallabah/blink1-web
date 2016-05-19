964e4579a05b026044db840d18434a2a213eb285
Peter Hamberg <peter.hamberg@academicwork.se>
2016-05-19 14:29:34 +0200

    add support for parsing post params in content use params to guide response make response in_channel and use correct content-type in response message

--- servercomponents/slack.js
+++ servercomponents/slack.js
@@ -11,40 +11,77 @@ function Slack(blink)
 	this.blink = blink;
 };
 
+function Params( str ){
+	function makeParam(d){
+		var decoded = decodeURIComponent(d);
+		var pivot = decoded.indexOf("=");
+		if( pivot == -1 )
+			return {name:decoded,value:undefined};
+		var name = decoded.substring(0,pivot);
+		var value = decoded.substring(pivot+1,decoded.length);
+		return {name:name,value:value};
+	};
+	this.data = str.split("&").map( makeParam );
+};
+Params.prototype.get = function( name ){
+	var found = this.data.filter( function(d){ return d.name === name } );
+	if( found.length === 0 )
+		return undefined;
+	return found[0].value;
+};
+
 Slack.prototype.post = function( m, response, device ){
+	var writeOkResponse = function( msg ){
+		response.writeHead(200, {"Content-Type": "application/json"});
+		response.write( JSON.stringify({
+			response_type: "in_channel",
+			text: msg
+		}));
+		response.end();
+	};
+	var findColor = function(msg){
+		var isHex = /#[A-F0-9]{6}/ig;
+		var hexcolor = isHex.exec( msg );
+		if( hexcolor == null || hexcolor.length == 0 )
+			return {
+				r: Math.floor(Math.random()*256),
+				g: Math.floor(Math.random()*256),
+				b: Math.floor(Math.random()*256)
+			};
+		return {
+			r: parseInt(hexcolor[0].substr(1,2),16),
+			g: parseInt(hexcolor[0].substr(3,2),16),
+			b: parseInt(hexcolor[0].substr(5,2),16)
+		}
+	}
+	if( !m ){
+		response.writeHead(400);
+		response.end();
+		return;
+	}
+	var params = new Params(m);
+	var token = params.get( "token" );
+	var user = params.get("user_name");
+	var text = params.get("text");
 	
-	/*if(
-		m.token != "R1UCfh6S6rRQXbp4MKnNntFW" ||
-		m.text == undefined ||
-		!m.text.toLowerCase().includes( "please" )
-		)
-		{
-			response.writeHead(200);
-			response.write( "you get nothing" );
-			response.end();
-			return;
-		}*/
-	var r = Math.floor(Math.random()*256);
-	var g = Math.floor(Math.random()*256);
-	var b = Math.floor(Math.random()*256);
+	if(!text.toLowerCase().includes( "please" ) )
+	{
+		writeOkResponse( user+", you get nothing!" );
+		return;
+	}
 	
-	
-	var hexcolor = tools.toColor( r, g, b );
+	var color = findColor( text );
+	var hexcolor = tools.toColor( color.r, color.g, color.b );
 	
 	this.blink(device).fadeRGB( {
 		fadeMillis:200,
-		r: r,
-		g: g,
-		b: b,
+		r: color.r,
+		g: color.g,
+		b: color.b,
 		ledn: device,
 		callback: function(opt) {
-			response.writeHead(200);
-			response.write( JSON.stringify(
-			{
-				response_type: "in_channel",
-				text: "you get "+hexcolor
-			} ) );
-			response.end();
+			console.log("slack set led to "+hexcolor);
+			writeOkResponse( user+", you get "+hexcolor );
 		}
 	});
 };
