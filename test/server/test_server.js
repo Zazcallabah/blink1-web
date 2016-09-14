fa387f6033f6ce4e85e063d99db389cb60a7e617
Peter Hamberg <zazcallabah@gmail.com>
2016-09-14 07:17:25 +0200

    try to fix chai-http, and test server

---
+++ test/server/test_server.js
@@ -0,0 +1,21 @@
+var chai = require('chai');
+var chaiHttp = require('chai-http');
+var server = require('../../server');
+var should = chai.should();
+var sinon = require('sinon');
+chai.use(chaiHttp);
+
+Blink1 = function(){};
+Blink1.prototype.devices = function(){return [{},{}]};
+
+
+describe('server', function() {
+	it('should return', function(done) {
+		chai.request('http://localhost:19333')
+			.get('/api/version')
+			.end(function(err, res){
+				res.should.have.status(200);
+				done();
+			});
+	});
+});
