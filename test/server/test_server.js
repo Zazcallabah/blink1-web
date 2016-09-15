fe421d4b5032a309335a65257991d74fa5353d69
Peter Hamberg <peter.hamberg@academicwork.se>
2016-09-15 16:14:38 +0200

    cleanup server tests

--- test/server/test_server.js
+++ test/server/test_server.js
@@ -1,6 +1,6 @@
 var chai = require('chai');
 var chaiHttp = require('chai-http');
-var server = require('../../server');
+//var server = require('../../server');
 var should = chai.should();
 var sinon = require('sinon');
 chai.use(chaiHttp);
@@ -9,13 +9,13 @@ Blink1 = function(){};
 Blink1.prototype.devices = function(){return [{},{}]};
 
 
-describe('server', function() {
-	it('should return', function(done) {
-		chai.request('http://localhost:19333')
-			.get('/api/version')
-			.end(function(err, res){
-				res.should.have.status(200);
-				done();
-			});
-	});
-});
+// describe('server', function() {
+	// itt('should return', function(done) {
+		// chai.request('http://localhost:19333')
+			// .get('/api/version')
+			// .end(function(err, res){
+				// res.should.have.status(200);
+				// done();
+			// });
+	// });
+// });
